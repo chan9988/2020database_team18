c2a19d711ed4ed7c062131166c6097ec07e060d6
chan <kai9988ckc.cs07@nctu.edu.tw>
2020-06-10 11:03:08 +0800

    add file

--- database_project.php
+++ database_project.php
@@ -1,34 +1,63 @@
 <HTML>
-        <HEAD>
-                <meta charset="utf-8">
-                <title> database_project </title>               
-        </HEAD>
-        <BODY>
+    <HEAD>
+        <meta charset="utf-8">
+        <title> database_project </title>
+		<style>
+			body {background-color:#FFFFDE;}
+			footer {color:white;background:black;text-align:center}
+			footer {position:fixed; bottom:0px; left:0px; width:100%}
+		</style>
+    </HEAD>
+    <BODY>
+        <h3>說明:</h3>
+		<p>本網頁可讓使用者選擇兩位球員，我們會分析這兩位球員在BBO這款遊戲中的數據表現，及真實的數據表現來分析這兩位球員。並同時會比較遊戲方比較偏袒哪位球員。</p><br>
                 <FORM method=post action=t1.php>
-                <p>Input the player id for query: <INPUT type=text name=ca> </p>
+		<br>
+		<p>Input player 1 information:</p>
+		<p> 	year:
+			<select name="year1">
+				<option></option>
+				<option> 2019 </option>
+			</select>
+		    	team:
+			<select name="team_name">
+				<option></option>
+				<option>Lamigo</option>
+                <option>中信兄弟</option>
+                <option>統一獅</option>
+                <option>富邦悍將</option>
+			</select>
+			player_name:
+			<select>
+			</select>
+		</p>
+		<p>Input player 2 information:</p>
+		<p> 	year:
+			<select name="year2">
+				<option></option>
+				<option> 2019 </option>
+			</select>
+		    	team:
+			<select name="team2">
+				<option></option>
+				<option>Lamigo</option>
+				<option>中信兄弟</option>
+				<option>統一獅</option>
+				<option>富邦悍將</option>
+			</select>
+		    	player_name:
+			<select name="playername2">
+			</select>
+		</p>
+		<!--
+		<p>Input the player id for query: <INPUT type=text name=ca> </p>
                 <p>Input the player id for query: <INPUT type=text name=cb> </p>
                 <p>Input the player id for query: <INPUT type=text name=cc> </p>
-                <p><INPUT type=submit value="submit">
-                <INPUT type=reset value="clear"></FORM>
-                <!--
-                <?php
-                        $servername = "localhost";
-                        $username = "user";
-                        $password = "user";
-                        $test = "select player_name from 19CPBLGame where player_id = 2";
-                        $a = 1;
-                        $conn = new mysqli($servername, $username, $password,'test');
-                        $result1=$conn->query("select player_name from 19CPBLGame where player_id=1");
-                        $result2=$conn->query("select player_name from 19CPBLGame where player_id = $a");
-                        $row=$result1->fetch_row();
-                        echo "$row[0]<br>";
-                        $row=$result2->fetch_row();
-                        echo "$row[0]<br>";
-                        if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                        }
-                        echo "Connected successfully";
-                ?> 
-                -->
-        </BODY>
+		-->		
+		<p><INPUT type=submit value="submit">
+		<INPUT type=reset value="clear"></FORM>
+		<footer>
+			<p> 2020 nctu database term project -- team 18</p>
+		</footer>
+    </BODY>
 <HTML>
--- t1.php
+++ t1.php
@@ -1,15 +1,22 @@
 <HTML>
-        <HEAD>
-                <meta charset="utf-8">
-                <title> database_project </title>               
-        </HEAD>
-        <BODY>
-                <!--
-                <p><?php echo $_POST[ca] ;?></p>
-                <p><?php echo $_POST[cb] ;?></p>
-                <p><?php echo $_POST[cc] ;?></p>
-                -->
+    <HEAD>
+        <meta charset="utf-8">
+        <title> database_project </title>
+		<style>
+            body {background-color:#FFFFDE;}
+            footer {color:white;background:black;text-align:center}
+            footer {position:fixed; bottom:0px; left:0px; width:100%}
+        </style>				
+    </HEAD>
+    <BODY>
+        <!--
+        <p><?php echo $_POST[ca] ;?></p>
+        <p><?php echo $_POST[cb] ;?></p>
+        <p><?php echo $_POST[cc] ;?></p>
+        -->
                 <?php
+						echo "$_POST[year1]<br>";
+						echo "$_POST[year2]<br>";
                         $servername = "localhost";
                         $username = "user";
                         $password = "user";
@@ -31,5 +38,8 @@
                         echo "The player where id = $_POST[cc] is $row[0]<br>";
                 ?>
 
-        </BODY>
+    </BODY>
+	<footer>
+        <p> 2020 nctu database term project -- team 18</p>
+    </footer>
 <HTML>
